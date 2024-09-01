7c1ece035fdddfefc2b6e976166347552666bb0e
David <d.huamanor@alum.up.edu.pe>
2024-09-01 17:03:15 -0500

    .

---
+++ Readme.md
@@ -0,0 +1,3 @@
+stockfish.js
+============
+[Chessboard.js](https://github.com/oakmac/chessboardjs) & [Chess.js](https://github.com/jhlywa/chess.js) GUI with [Stockfish.js](https://github.com/lichess-org/stockfish.js/tree/ddugovic) engine compiled to JavaScript.
---
+++ test.js
@@ -0,0 +1,102 @@
+import { Chess } from 'https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.13.4/chess.js';
+
+var game = new Chess()
+
+var board = null
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+
+var wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+var stockfish = new Worker(wasmSupported ? 'stockfish.wasm.js' : 'stockfish.js');
+
+stockfish.addEventListener('message', function(e) {
+  if (e.data.startsWith('bestmove')) {
+    var bestMove = e.data.split(' ')[1];
+    game.move({ from: bestMove.slice(0, 2), to: bestMove.slice(2, 4), promotion: 'q' });
+    board.position(game.fen());
+    updateStatus();
+  }
+});
+
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+}
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  updateStatus()
+
+  stockfish.postMessage('position fen ' + game.fen());
+  stockfish.postMessage('go depth 10');
+
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+function updateStatus () {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html(game.pgn())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+  pieceTheme: 'img/chesspieces/wikipedia/{piece}.png'
+
+}
+board = Chessboard('board', config)
+
+updateStatus()
